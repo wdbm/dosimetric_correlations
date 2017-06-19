d7764c8b5dd81756b0a326ff1b5c6765a003667a
Will Breaden Madden <wbm@protonmail.ch>
2017-06-20 00:35:38 +0100

    add preprocess_CSV_file.py, add cures_cancer_1.py, add README.md

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # dosimetric_correlations
+
+# note
+
+This project is a work in progress.
+
+# setup
+
+Install the following:
+
+- NumPy
+- pandas
+- Scikit-learn
+- TensorBoard
+- TensorFlow
+
+# use
+
+The input data should be CSV with the following fields:
+
+- features:
+    - PTV vol (cc)
+    - Lungs-GTV vol (cc)
+    - Lungs-GTV-PTV vol (cc)
+    - Lungs-GTV in PTV vol (cc)
+    - KBP Lungs (cc)
+    - Lungs-GTV - KBP Lungs (cc)
+    - Heart vol (cc)
+    - Heart in PTV vol (cc)
+- targets:
+    - V5 (%)
+    - V20 (%)
+    - Mean Lungs-GTV (Gy)
+    - V30 (%)
+    - Mean (Gy)
+
+The rightmost columns should be the targets.
+
+Manually remove missing values from data.
+
+Preprocess the CSV data such that all features are scaled to (-1, 1):
+
+```Bash
+./preprocess_CSV_file.py --infile=data.csv --outfile=preprocessed_data.csv
+```
+
+Train and evaluate on preprocessed CSV data:
+
+```Bash
+cures_cancer_1.py --help
+
+cures_cancer_1.py --infile=preprocessed_data.csv
+```
+
+The basic accuracy evaluation is a printout of the percentage differences of the model hypothesis output values from input data values.
---
+++ preprocess_CSV_file.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+"""
+################################################################################
+#                                                                              #
+# preprocess_CSV_file                                                          #
+#                                                                              #
+################################################################################
+#                                                                              #
+# LICENCE INFORMATION                                                          #
+#                                                                              #
+# This program accesses a CSV file and preprocesses the data in it to attempt  #
+# to make it suitable for machine learning algorithms.                         #
+#                                                                              #
+# copyright (C) 2017 William Breaden Madden                                    #
+#                                                                              #
+# This software is released under the terms of the GNU General Public License  #
+# version 3 (GPLv3).                                                           #
+#                                                                              #
+# This program is free software: you can redistribute it and/or modify it      #
+# under the terms of the GNU General Public License as published by the Free   #
+# Software Foundation, either version 3 of the License, or (at your option)    #
+# any later version.                                                           #
+#                                                                              #
+# This program is distributed in the hope that it will be useful, but WITHOUT  #
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or        #
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for     #
+# more details.                                                                #
+#                                                                              #
+# For a copy of the GNU General Public License, see                            #
+# <http://www.gnu.org/licenses/>.                                              #
+#                                                                              #
+################################################################################
+
+usage:
+    program [options]
+
+options:
+    -h, --help               display help message
+    --version                display version and exit
+
+    --infile=FILENAME        CSV input file  [default: output.csv]
+    --outfile=FILENAME       CSV output file [default: output_preprocessed.csv]
+"""
+
+from __future__ import division
+import docopt
+import os
+
+import pandas as pd
+import sklearn.preprocessing
+
+name    = "preprocess_CSV_file"
+version = "2017-06-19T2122Z"
+logo    = None
+
+def main(options):
+
+    print("")
+
+    filename_CSV_input  = options["--infile"]
+    filename_CSV_output = options["--outfile"]
+
+    if not os.path.isfile(os.path.expandvars(filename_CSV_input)):
+        log.error("file {filename} not found".format(
+            filename = filename_CSV_input
+        ))
+        exit()
+
+    print("read CSV from {filename}".format(filename = filename_CSV_input))
+    data = pd.read_csv(filename_CSV_input)
+
+    scaler = sklearn.preprocessing.MinMaxScaler(feature_range = (-1, 1))
+
+    number_of_columns          = data.shape[1]
+    indices_of_feature_columns = range(0, number_of_columns)
+
+    # scale feature columns
+    print("scale features")
+    data[indices_of_feature_columns] = scaler.fit_transform(data[indices_of_feature_columns])
+
+    print("save scaled CSV to {filename}".format(filename = filename_CSV_output))
+    data.to_csv(
+        filename_CSV_output,
+        index  = False,
+        #header = False
+    )
+
+if __name__ == "__main__":
+    options = docopt.docopt(__doc__)
+    if options["--version"]:
+        print(version)
+        exit()
+    main(options)
